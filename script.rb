eb8b6b57a20ad6100017c49fc927b6c9908ed76d
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-05-20 18:13:06 +0200

    Adding a feature that permits us to get every image of each project

--- script.rb
+++ script.rb
@@ -6,20 +6,25 @@ require 'websocket/driver'
 
 uri  = URI("https://opensourcepolitics.eu/references-clients/")
 body  = Net::HTTP.get(uri)
+i = 1
 
 document = Nokogiri::HTML(body)
 projects = document.css('a')
 
+
 projects.each do |project|
   next unless project['href'].include?('/project/')
-  puts project['href']
+  image = project.css('img').attribute('data-lazy-srcset').value.split(' ')[0]
   uri = URI(project['href'])
   body = Net::HTTP.get(uri)
   document = Nokogiri::HTML(body)
   project_name = document.css('h1').text
   open('projects.txt', 'a') do |f|
-    f.write project_name + "\n"
+    f.write "#{i} Name :   #{project_name} \n"
+    f.write "Image :  #{image} \n"
   end
+
+  i += 1
 end
 
 
