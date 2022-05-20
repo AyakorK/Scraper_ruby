f41282134964c77e8f0dba8d26a0d34ce04caea2
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-05-20 17:51:48 +0200

    Adding a feature that permits us to get every name of each project

--- script.rb
+++ script.rb
@@ -13,6 +13,13 @@ projects = document.css('a')
 projects.each do |project|
   next unless project['href'].include?('/project/')
   puts project['href']
+  uri = URI(project['href'])
+  body = Net::HTTP.get(uri)
+  document = Nokogiri::HTML(body)
+  project_name = document.css('h1').text
+  open('projects.txt', 'a') do |f|
+    f.write project_name + "\n"
+  end
 end
 
 
