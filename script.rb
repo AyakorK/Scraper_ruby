05dfe11e2a982157f2ef82315520d2f1a97a70a8
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-05-23 14:52:30 +0200

    Factorize code into functions

--- script.rb
+++ script.rb
@@ -7,21 +7,47 @@ require 'nokogiri'
 require 'websocket/driver'
 require 'csv'
 
+def find_unique_link(links, variable, unique_link)
+  links.each do |link|
+    href =  link['href'].to_s
+
+    next if check_if_valid(href, variable)
+    next if i == 4 && href.downcase.include?('opensource') || !href.start_with?('https')
+
+    unless unique_link.include?(link['href']) || unique_link.include?(link['href'].chomp('/'))
+      (unique_link[i] = link['href']) and break
+    end
+  end
+end
+
+def check_if_valid(href, variable)
+  (href.downcase.include?('open') && variable != 4) || !(href.end_with?('.fr') ||
+    href.end_with?('.is') || href.end_with?('.be') ||
+    href.end_with?('.brussels') || href.end_with?('.paris') ||
+    (href.count('/') == 3 && href.gsub(/[[:space:]]/, '').end_with?('/') ||
+      href.downcase.include?('participez') || href.downcase.include?('democratie'))) ||
+    !href.start_with?('https')
+end
+
+def get_all_links(document)
+  document.css('a')
+end
+
 uri = URI('https://opensourcepolitics.eu')
 body = Net::HTTP.get(uri)
 # Navigate to "https://opensourcepolitics.eu/references-clients/" by clicking on the "Client" button
 doc = Nokogiri::HTML(body)
 data = []
-doc.css('a').each do |first_link|
+i = 1
+get_all_links(doc).each do |first_link|
   next unless first_link.text == 'Clients'
 
   client_link = first_link['href']
-  i = 1
 
   uri = URI(client_link)
   body = Net::HTTP.get(uri)
   document = Nokogiri::HTML(body)
-  projects = document.css('a')
+  projects = get_all_links(document)
   unique_link = {}
 
   projects.each do |project|
@@ -30,26 +56,18 @@ doc.css('a').each do |first_link|
     uri = URI(project['href'])
     body = Net::HTTP.get(uri)
     project_document = Nokogiri::HTML(body)
-    links = project_document.css('a')
+    links = get_all_links(project_document)
 
     project_name = project_document.css('h1').text.gsub(/[[:space:]]/, ' ')
+
     image = project.css('img').attribute('data-lazy-srcset').value.split(' ')[0]
-    links.each do |link|
-      href =  link['href'].to_s
-      next if (href.downcase.include?('open') && i != 4) || !(href.end_with?('.fr') ||
-        href.end_with?('.is') || href.end_with?('.be') ||
-        href.end_with?('.brussels') || href.end_with?('.paris') ||
-        (href.count('/') == 3 && href.gsub(/[[:space:]]/, '').end_with?('/') ||
-          href.downcase.include?('participez') || href.downcase.include?('democratie'))) ||
-              !href.start_with?('https')
-      next if i == 4 && href.downcase.include?('opensource') || !href.start_with?('https')
-      unless unique_link.include?(link['href']) || unique_link.include?(link['href'].chomp('/'))
-        (unique_link[i] =
-           link['href']) and break
-      end
-    end
+
+    find_unique_link(links, i, unique_link)
+
     puts "#{unique_link.length} / #{i}"
+
     data << [project_name, image, unique_link[i]].join(',')
+
     i += 1
   end
 end
